3c49957ee2fd8437aff4bc94471cb098ca48d103
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-07-22 15:40:45 +0400

    Reset password

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 import express from 'express';
 
-import { register, registerConfirm, login, forgotPassword } from '../controllers/auth.js';
+import { register, registerConfirm, login, forgotPassword, resetPassword } from '../controllers/auth.js';
 
-import { loginValidator, registerValidator, forgotPasswordValidator } from '../validators/auth.js';
+import {
+  loginValidator,
+  registerValidator,
+  forgotPasswordValidator,
+  resetPasswordValidator
+} from '../validators/auth.js';
 
 const router = express.Router();
 
@@ -10,6 +15,7 @@ router
   .post('/register', registerValidator, register)
   .post('/register/confirm/:confirmationToken', registerConfirm)
   .post('/login', loginValidator, login)
-  .post('/password/forgot', forgotPasswordValidator, forgotPassword);
+  .post('/password/forgot', forgotPasswordValidator, forgotPassword)
+  .post('/password/reset/:resetPasswordToken', resetPasswordValidator, resetPassword);
 
 export default router;
--- validators/auth.js
+++ validators/auth.js
@@ -59,4 +59,24 @@ const forgotPasswordValidator = validation([
     .withMessage('message=Please add a valid email;type=INVALID_EMAIL')
 ]);
 
-export { registerValidator, loginValidator, forgotPasswordValidator };
+const resetPasswordValidator = validation([
+  body('password')
+    .notEmpty()
+    .withMessage('message=Please add a password;type=NO_PASSWORD')
+    .isLength({ min: process.env.PASSWORD_MIN_LENGTH })
+    .withMessage(
+      `message=The password has to have at least ${process.env.PASSWORD_MIN_LENGTH} characters;type=PASSWORD_MIN_LENGTH`
+    ),
+  body('repeatedPassword')
+    .notEmpty()
+    .withMessage('message=Please add the repeated password;type=NO_REPEATED_PASSWORD')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error(`message=The repeated password doesn't match the password;type=REPEATED_PASSWORD_NO_MATCH`);
+      }
+
+      return true;
+    })
+]);
+
+export { registerValidator, loginValidator, forgotPasswordValidator, resetPasswordValidator };
