23f15631140d61e555ea0a8b968f06f30e2d0f04
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-09-12 18:04:16 +0400

    Get offer

--- controllers/offer.js
+++ controllers/offer.js
@@ -37,6 +37,107 @@ const getFeatures = async (req, res, next) => {
 };
 
 /**
+ * @api {GET} /offer/:offerId Get Offer
+ * @apiGroup Offer
+ * @apiName OfferGet
+ *
+ * @apiDescription Get an offer.
+ *
+ * @apiParam {Number} offerId The offer id
+ *
+ * @apiSuccessExample Success Example
+ * {
+ *   "id": 14,
+ *   "type_of_good": "apartment",
+ *   "transaction_type": "to-rent",
+ *   "square_meters": 24,
+ *   "nb_rooms": 2,
+ *   "nb_bedrooms": 1,
+ *   "price": 8000,
+ *   "description": "Lorem ipsum...",
+ *   "city_id": 67865,
+ *   "images": [
+ *     "img.r3tests.net/good-properties/29/1726064100370.jpeg",
+ *     "img.r3tests.net/good-properties/29/1726064100449.jpeg"
+ *   ],
+ *   "features": {
+ *     "general": [
+ *       "Close to shops"
+ *     ],
+ *     "indoor": [
+ *       "Air conditioning",
+ *       "Internet/Wi-Fi"
+ *     ],
+ *     "outdoor": [
+ *       "Garage"
+ *     ]
+ *   },
+ *   "contact": {
+ *     "email": "alicia.meller@example.com",
+ *     "telephone": "",
+ *     "whatsapp": ""
+ *   }
+ * }
+ *
+ * @apiPermission Public
+ */
+const getOffer = async (req, res, next) => {
+  const { offerId } = req.params;
+  let result;
+
+  try {
+    result = await dbUtil.transaction(async (transaction) => {
+      const offer = await Offer.findOne({ where: { id: offerId } }, { transaction });
+      const offerImages = await OfferImage.findAll({ where: { offer_id: offerId } }, { transaction });
+      const offerFeaturesIds = (
+        await OfferOfferFeature.findAll({ where: { offer_id: offerId }, raw: true }, { transaction })
+      ).map((offerFeatureId) => offerFeatureId.offer_feature_id);
+      const offerFeatures = await OfferFeature.findAll({ where: { id: offerFeaturesIds } });
+      const contact = await Contact.findOne({ where: { user_id: offer.user_id } });
+
+      return { offer, offerImages, offerFeatures, contact };
+    });
+  } catch {
+    return next(new ErrorResponse('Cannot find offer', httpStatus.NOT_FOUND, 'NOT_FOUND'));
+  }
+
+  const offer = {
+    id: result.offer.id,
+    type_of_good: result.offer.type_of_good,
+    transaction_type: result.offer.transaction_type,
+    square_meters: result.offer.square_meters,
+    nb_rooms: result.offer.nb_rooms,
+    nb_bedrooms: result.offer.nb_bedrooms,
+    price: result.offer.price,
+    description: result.offer.description,
+    city_id: result.offer.city_id,
+    images: result.offerImages.map((image) => image.link),
+    features: {
+      general: [],
+      indoor: [],
+      outdoor: []
+    },
+    contact: {
+      email: result.contact.email,
+      telephone: result.contact.telephone,
+      whatsapp: result.contact.whatsapp
+    }
+  };
+
+  for (const feature of result.offerFeatures) {
+    if (feature.type === 'general') {
+      offer.features.general.push(feature.name);
+    } else if (feature.type === 'indoor') {
+      offer.features.indoor.push(feature.name);
+    } else {
+      offer.features.outdoor.push(feature.name);
+    }
+  }
+
+  res.status(httpStatus.OK).json(offer);
+};
+
+/**
  * @api {POST} /offer Create Offer
  * @apiGroup Offer
  * @apiName OfferCreate
@@ -134,4 +235,4 @@ const createOffer = async (req, res, next) => {
   res.status(httpStatus.OK).json({ id: result.offer.id });
 };
 
-export { getFeatures, createOffer };
+export { getFeatures, getOffer, createOffer };
--- routes/offer.js
+++ routes/offer.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createOffer, getFeatures } from '../controllers/offer.js';
+import { createOffer, getFeatures, getOffer } from '../controllers/offer.js';
 
 import authorize from '../middlewares/authorize.js';
 
@@ -8,6 +8,6 @@ import { createOfferValidator } from '../validators/offer.js';
 
 const router = express.Router();
 
-router.get('/features', getFeatures).post('/', authorize, createOfferValidator, createOffer);
+router.get('/features', getFeatures).get('/:offerId', getOffer).post('/', authorize, createOfferValidator, createOffer);
 
 export default router;
