e6211972529e5fde7205c9d38f8bc586df0bb7f5
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-10-25 11:25:16 +0400

    Get article

--- controllers/article.js
+++ controllers/article.js
@@ -3,6 +3,8 @@ import { Sequelize } from 'sequelize';
 
 import Article from '../models/Article.js';
 
+import ErrorResponse from '../classes/ErrorResponse.js';
+
 /**
  * @api {GET} /article/all Get Articles
  * @apiGroup Article
@@ -34,11 +36,43 @@ const getAllArticles = async (req, res, next) => {
       'author',
       'date'
     ],
-    order: [['date', 'DESC']],
-    logging: console.log
+    order: [['date', 'DESC']]
   });
 
   res.status(httpStatus.OK).json(articles);
 };
 
-export { getAllArticles };
+/**
+ * @api {GET} /article/:articleId Get Article
+ * @apiGroup Article
+ * @apiName ArticleGet
+ *
+ * @apiDescription Get a single article.
+ *
+ * @apiParam {Number} articleId The article id
+ *
+ * @apiSuccessExample Success Example
+ * {
+ *   "id": 12,
+ *   "title": "Title of the article",
+ *   "content": "Lorem ipsum odor amet, consectetuer adipiscing elit",
+ *   "image": "https://img.website.com/abcd.jpg",
+ *   "author": "Tom Boss",
+ *   "date": "2024-10-16T14:07:55.370Z"
+ * }
+ *
+ * @apiError (Error (404)) NOT_FOUND Cannot find article
+ * @apiPermission Public
+ */
+const getArticle = async (req, res, next) => {
+  const { articleId } = req.params;
+  const article = await Article.findAll({ where: { id: articleId } });
+
+  if (article.length === 0) {
+    return next(new ErrorResponse('Cannot find article', httpStatus.NOT_FOUND, 'NOT_FOUND'));
+  }
+
+  res.status(httpStatus.OK).json(article);
+};
+
+export { getAllArticles, getArticle };
--- routes/article.js
+++ routes/article.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
-import { getAllArticles } from '../controllers/article.js';
+import { getAllArticles, getArticle } from '../controllers/article.js';
 
 const router = express.Router();
 
-router.get('/all', getAllArticles);
+router.get('/all', getAllArticles).get('/:articleId', getArticle);
 
 export default router;
